0f4aa1d84d5df6efd0b39fa10a82d580a84d4faa
Alonso del Arte <alonso.delarte@gmail.com>
2020-07-05 13:19:59 -0400

    It's starting to work now

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ASCII Sokoban
 
 WORK IN PROGRESS: This is just going to be a quick and dirty ASCII implementation of Sokoban, using HTML, CSS and JavaScript. I'll start with ES5, will probably run into trouble, have to refactor to ES6 and still run into problems. I believe these problems will help clarify certain details in my mind as I set to work on a more structured, object-oriented implementation in Scala.
+
+Quick and dirty also means that I probably won't be using pure functions or immutable data structures.
+
+This implementation will not provide any warnings for having too many boxes or too few boxes in regards to goals. If a board has more than one player token, the last occurring player token will be recognized as such, but the previous player tokens will remain on the board; I make no guarantees whatseover about the functioning of the program in such a situation.
+
+For now there is no undo, but I might implement it later. I might also do some refactoring, but I'll probably get to a point of deciding this is good enough for quick and dirty and leave it at that.
+
+I also have this feeling I've gotten dyslexic about coordinates.
+
+I've tested this in Firefox and it should work in Chrome. It might even work in Edge. I make no promises whatsoever about Internet Explorer.
--- js/game.js
+++ js/game.js
@@ -1,15 +1,227 @@
-function pushBox() {
-    //
+var initBoardElem, boardElem, boardDiv;
+var initialState = [], currentState = [];
+var playerX, playerY;
+var moveCount = 0, boxCount = 0, boxOnGoalCount = 0;;
+
+function queryPosition(x, y) {
+    return currentState[x][y];
 }
 
-function move() {
-    //
+function changePosition(x, y, token) {
+    currentState[x][y] = token;
 }
 
-function setUpBoard() {
-    //
+function canPush(absX, absY, relX, relY) {
+    var currBoxToken = queryPosition(absX, absY);
+    var proposedX = absX + relX;
+    var proposedY = absY + relY;
+    switch (queryPosition(proposedX, proposedY)) {
+        case " ":
+            if (currBoxToken == "*") {
+                changePosition(absX, absY, ".");
+            } else {
+                changePosition(absX, absY, " ");
+            }
+            changePosition(proposedX, proposedY, "$");
+            return true;
+        case ".":
+            if (currBoxToken == "*") {
+                changePosition(absX, absY, ".");
+            } else {
+                changePosition(absX, absY, " ");
+            }
+            changePosition(proposedX, proposedY, "*");
+            return true;
+        default:
+            return false;
+    }
+}
+
+function canMove(relativeX, relativeY) {
+    var currPlayerToken = queryPosition(playerX, playerY);
+    if (relativeX == 0 && relativeY == 0) {
+        return false;
+    }
+    var absoluteX = playerX + relativeX;
+    var absoluteY = playerY + relativeY;
+    var playerToken = "@";
+    switch (queryPosition(absoluteX, absoluteY)) {
+        case "*":
+            playerToken = "+";
+        case "$":
+            if (canPush(absoluteX, absoluteY, relativeX, relativeY)) {
+                if (currPlayerToken == "@") {
+                    changePosition(playerX, playerY, " ");
+                } else {
+                    changePosition(playerX, playerY, ".");
+                }
+                playerX = absoluteX;
+                playerY = absoluteY;
+                changePosition(playerX, playerY, playerToken);
+                return true;
+            }
+            return false;
+        case " ":
+            if (currPlayerToken == "@") {
+                changePosition(playerX, playerY, " ");
+            } else {
+                changePosition(playerX, playerY, ".");
+            }
+            playerX = absoluteX;
+            playerY = absoluteY;
+            changePosition(playerX, playerY, "@");
+            return true;
+        case ".":
+            if (currPlayerToken == "@") {
+                changePosition(playerX, playerY, " ");
+            } else {
+                changePosition(playerX, playerY, ".");
+            }
+            playerX = absoluteX;
+            playerY = absoluteY;
+            changePosition(playerX, playerY, "+");
+            return true;
+        default:
+            return false;
+    }
+}
+
+function processArrowKey(e) {
+    var x, y;
+    switch(e.keyCode) {
+        case 37:
+            x = 0;
+            y = -1;
+            break;
+        case 38:
+            x = -1;
+            y = 0;
+            break;
+        case 39:
+            x = 0;
+            y = 1;
+            break;
+        case 40:
+            x = 1;
+            y = 0;
+            break;
+        default:
+            x = 0;
+            y = 0;
+    }
+    if (canMove(x, y)) {
+        moveCount++;
+        writeBoard();
+    }
+}
+
+function parseBoard(boardStr) {
+    var parsedSoFar = [];
+    var rowsSoFar = [];
+    var character;
+    var row = 0;
+    var col = 0;
+    for (var i = 0; i < boardStr.length; i++) {
+        character = boardStr.charAt(i);
+        switch (character) {
+            case "#":
+                parsedSoFar.push("#");
+                col++;
+                break;
+            case " ":
+            case "-":
+            case "_":
+                parsedSoFar.push(" ");
+                col++;
+                break;
+            case "@":
+            case "p":
+                parsedSoFar.push("@");
+                playerX = row;
+                playerY = col;
+                col++;
+                break;
+            case "+":
+            case "P":
+                parsedSoFar.push("+");
+                playerX = row;
+                playerY = col;
+                col++;
+                break;
+            case "$":
+            case "b":
+                parsedSoFar.push("$");
+                col++;
+                boxCount++;
+                break;
+            case "*":
+            case "B":
+                parsedSoFar.push("*");
+                col++;
+                boxCount++;
+                boxOnGoalCount++;
+                break;
+            case ".":
+                parsedSoFar.push(".");
+                col++;
+                break;
+            case ";":
+                rowsSoFar.push(parsedSoFar);
+                parsedSoFar = [];
+                col = 0;
+                row++;
+        }
+    }
+    if (parsedSoFar.length > 0) {
+        rowsSoFar.push(parsedSoFar);
+    }
+    return rowsSoFar;
+}
+
+function writeBoard() {
+    var brd = "";
+    for (var row = 0; row < currentState.length; row++) {
+        brd = brd + "<tr>";
+        for (var col = 0; col < currentState[row].length; col++) {
+            brd = brd + "<td class=\"";
+            switch (currentState[row][col]) {
+                case " ":
+                    brd = brd + "floor\"> ";
+                    break;
+                case "#":
+                    brd = brd + "wall\">#";
+                    break;
+                case "@":
+                    brd = brd + "pusher\">@";
+                    break;
+                case "+":
+                    brd = brd + "pusherOnGoal\">+";
+                    break;
+                case "$":
+                    brd = brd + "box\">$";
+                    break;
+                case "*":
+                    brd = brd + "boxOnGoal\">*";
+                    break;
+                case ".":
+                    brd = brd + "goal\">.";
+            }
+            brd = brd + "</td>";
+        }
+        brd = brd + "</tr>";
+    }
+    boardElem.innerHTML = brd;
 }
 
 function initialize() {
-  //
+    initBoardElem = document.getElementsByTagName("pre").item(0);
+    initialState = parseBoard(initBoardElem.innerHTML);
+    console.log(initialState);
+    currentState = initialState;
+    boardElem = document.createElement("table");
+    document.onkeydown = processArrowKey;
+    boardDiv = document.getElementById("board");
+    boardDiv.appendChild(boardElem);
+    initBoardElem.style.display = "none";
+    writeBoard();
 }
